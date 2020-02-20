2bae1fef56df97cd94b50f31abb5d65fbe854c7d
TabrisXing <xingtianx@gmail.com>
2020-02-19 22:36:20 -0500

    make player move smooth

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class main : MonoBehaviour
 {
+    public float speed;
+    public float jumpheight;
+
+    private Rigidbody rb;
+    private bool groundContact;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        groundContact = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 up = new Vector3(0.0f, jumpheight, 0.0f);
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown("space") && groundContact)
+        {
+            rb.AddForce(up);
+            groundContact = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Ground")
+        {
+            groundContact = true;
+        }
     }
 }
