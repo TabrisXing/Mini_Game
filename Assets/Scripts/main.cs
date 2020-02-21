53c09447656220ec4723a53cce87e71fa8636d31
TabrisXing <xingtianx@gmail.com>
2020-02-20 19:54:43 -0500

    add scores and highest scores

--- Assets/Scripts/main.cs
+++ Assets/Scripts/main.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class main : MonoBehaviour
 {
     public float speed;
     public float jumpheight;
     public GameObject jiguanprefab;
+    public GameObject pickup;
     public float secondsBetweenjiguan;
+    public Text scoresText;
 
     private Rigidbody rb;
     private bool isGrounded;
+    private int points;
 
 
     // Start is called before the first frame update
@@ -59,6 +64,27 @@ public class main : MonoBehaviour
     {
         GameObject jiguan = Instantiate<GameObject>(jiguanprefab);
         jiguan.transform.position = new Vector3(transform.position.x + 35.0f, -9.0f, 0f);
+        GameObject pickups = Instantiate<GameObject>(pickup);
+        for (int i = 0; i < 10; i++)
+        {
+            float angle = i * Mathf.PI * 2f / 12;
+            Vector3 newPos = new Vector3(Mathf.Cos(angle) * 8f + jiguan.transform.position.x, Mathf.Sin(angle) * 8f -9.0f, 0f);
+            Instantiate(pickup, newPos, Quaternion.identity);
+        }
         Invoke("jiguan", secondsBetweenjiguan);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+            points = points + 100;
+            scoresText.text = "Scores: " + points.ToString();
+            if (points > HighScore.score)
+            {
+                HighScore.score = points;
+            }
+        }
     }
 }
